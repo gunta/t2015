beb0c540d87893f031032198e404f48b83a97ed1
Gunther Brunner <gunta85@gmail>
2015-11-13 15:43:54 +0900

    first query

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,27 @@ var router = express.Router()
 var r = require('rethinkdb')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
+router.get('/', function (req, res, next) {
+
+  r.table('users').run(req._rdbConn).then(function () {
+
+  }).then(function (result) {
+    res.send(JSON.stringify(result))
+  }).error(handleError(res))
+    .finally(next)
+
+  res.send('respond with a resource 2')
+
+
 })
+
+function get(req, res, next) {
+  r.table('todos').orderBy({index: "createdAt"}).run(req._rdbConn).then(function (cursor) {
+    return cursor.toArray();
+  }).then(function (result) {
+    res.send(JSON.stringify(result));
+  }).error(handleError(res))
+    .finally(next);
+}
 
 module.exports = router
