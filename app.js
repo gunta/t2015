28b786b8d654f7dc19a2d8cccf7501f82cc0eec5
Gunther Brunner <gunta85@gmail>
2015-11-13 15:08:37 +0900

    Connect to rethink

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
+var config = require('./config')
+
 var routes = require('./routes/index')
 var users = require('./routes/users')
+
+var r = require('rethinkdb')
 
 var app = express()
 
@@ -22,8 +26,11 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(createConnection)
+
 app.use('/', routes)
 app.use('/users', users)
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -55,6 +62,31 @@ app.use(function (err, req, res, next) {
     error: {}
   })
 })
+
+
+function createConnection(req, res, next) {
+  r.connect(config.rethinkdb).then(function(conn) {
+    req._rdbConn = conn
+    next()
+  }).error(handleError(res))
+}
+
+/*
+ * Close the RethinkDB connection
+ */
+function closeConnection(req, res, next) {
+  req._rdbConn.close()
+}
+
+r.connect(config.rethinkdb, function(err, conn) {
+  if (err) {
+    console.log("Could not open a connection to initialize the database")
+    console.log(err.message)
+    process.exit(1)
+  }
+})
+
+app.use(closeConnection)
 
 
 module.exports = app
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rethinkdb: {
+    host: "172.30.2.80", // local
+    //host: "52.192.129.236", // remote
+    port: 28015,
+    authKey: "",
+    db: "test"
+  },
+  express: {
+    port: 8080
+  }
+}
