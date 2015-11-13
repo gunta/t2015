478bdb77629158f12f69ddc14665c7bb68332ae8
Gunther Brunner <gunta85@gmail>
2015-11-13 18:28:57 +0900

    no error

--- app.js
+++ app.js
@@ -34,54 +34,47 @@ app.use('/searchUser', users)
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+//app.use(function (req, res, next) {
+//  var err = new Error('Not Found')
+//  err.status = 404
+//  next(err)
+//})
 
-app.use(function(err, req, res, next){
-  res.status(err.status || 500)
-  res.send({
-    message: err.message,
-    error: err
-  })
-  return
-})
+
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500)
-    res.send({
-      message: err.message,
-      error: err
-    })
-  })
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500)
-  res.send({
-    message: err.message,
-    error: {}
-  })
-})
+//if (app.get('env') === 'development') {
+//  app.use(function (err, req, res, next) {
+//    res.status(err.status || 500)
+//    res.send({
+//      message: err.message,
+//      error: err
+//    })
+//  })
+//}
+//
+//// production error handler
+//// no stacktraces leaked to user
+//app.use(function (err, req, res, next) {
+//  res.status(err.status || 500)
+//  res.send({
+//    message: err.message,
+//    error: {}
+//  })
+//})
 
 
 /*
  * Send back a 500 error
  */
-function handleError(res) {
-  return function(error) {
-    res.send(500, {error: error.message});
-  }
-}
+//function handleError(res) {
+//  return function(error) {
+//    res.send(500, {error: error.message});
+//  }
+//}
 
 
 function createHelpers (req, res, next) {
